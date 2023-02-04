d359ebbbfdcbf15e6c766aa1291df195597a3f32
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2023-02-04 21:04:07 +0000

    add a QMC test

--- src/predicate_optimizer/quine_mccluskey_test.cpp
+++ src/predicate_optimizer/quine_mccluskey_test.cpp
@@ -17,7 +17,9 @@ TEST_CASE("Quine-mcCluskey", "") {
             {"10"_b, mask},
             {"11"_b, mask},
         };
-        std::unordered_set<Minterm> expectedResult{{"10"_b, "10"_b}};
+        std::unordered_set<Minterm> expectedResult{
+            {"10"_b, "10"_b},
+        };
 
         auto result = quine_mccluskey(std::move(minterms));
         REQUIRE(expectedResult == result);
@@ -25,8 +27,13 @@ TEST_CASE("Quine-mcCluskey", "") {
 
     SECTION("ABC | A~BC = AC") {
         Bitset mask{"111"};
-        std::vector<Minterm> minterms{{"111"_b, mask}, {"101"_b, mask}};
-        std::unordered_set<Minterm> expectedResult{{"101"_b, "101"_b}};
+        std::vector<Minterm> minterms{
+            {"111"_b, mask},
+            {"101"_b, mask},
+        };
+        std::unordered_set<Minterm> expectedResult{
+            {"101"_b, "101"_b},
+        };
 
         auto result = quine_mccluskey(std::move(minterms));
         REQUIRE(expectedResult == result);
@@ -34,8 +41,15 @@ TEST_CASE("Quine-mcCluskey", "") {
 
     SECTION("ABC | A~BC | AB~C = AC | AB") {
         Bitset mask{"111"};
-        std::vector<Minterm> minterms{{"111"_b, mask}, {"101"_b, mask}, {"110"_b, mask}};
-        std::unordered_set<Minterm> expectedResult{{"101"_b, "101"_b}, {"110"_b, "110"_b}};
+        std::vector<Minterm> minterms{
+            {"111"_b, mask},
+            {"101"_b, mask},
+            {"110"_b, mask},
+        };
+        std::unordered_set<Minterm> expectedResult{
+            {"101"_b, "101"_b},
+            {"110"_b, "110"_b},
+        };
 
         auto result = quine_mccluskey(std::move(minterms));
         REQUIRE(expectedResult == result);
@@ -44,8 +58,14 @@ TEST_CASE("Quine-mcCluskey", "") {
     SECTION("~A~B~C~D | ~A~B~CD | ~AB~C~D | ~AB~CD = ~A~C") {
         Bitset mask{"1111"};
         std::vector<Minterm> minterms{
-            {"0000"_b, mask}, {"0001"_b, mask}, {"0100"_b, mask}, {"0101"_b, mask}};
-        std::unordered_set<Minterm> expectedResult{{"0000"_b, "1010"_b}};
+            {"0000"_b, mask},
+            {"0001"_b, mask},
+            {"0100"_b, mask},
+            {"0101"_b, mask},
+        };
+        std::unordered_set<Minterm> expectedResult{
+            {"0000"_b, "1010"_b},
+        };
 
         auto result = quine_mccluskey(std::move(minterms));
         REQUIRE(expectedResult == result);
@@ -66,6 +86,28 @@ TEST_CASE("Quine-mcCluskey", "") {
         REQUIRE(expectedResult == result);
     }
 
-    std::unordered_set<Minterm> mtset;
+    SECTION("") {
+        Bitset mask{"111"};
+        std::vector<Minterm> minterms{
+            {"000"_b, mask},
+            {"010"_b, mask},
+            {"100"_b, mask},
+            {"011"_b, mask},
+            {"101"_b, mask},
+            {"111"_b, mask},
+        };
+
+        std::unordered_set<Minterm> expectedResult{
+            {"000"_b, "101"_b},
+            {"000"_b, "011"_b},
+            {"010"_b, "110"_b},
+            {"100"_b, "110"_b},
+            {"011"_b, "011"_b},
+            {"101"_b, "101"_b},
+        };
+
+        auto result = quine_mccluskey(std::move(minterms));
+        REQUIRE(expectedResult == result);
+    }
 }
 }  // namespace predicate_optimizer
